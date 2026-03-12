b5b855096a5863b01e8997289393fbf33692a550
Jagan Pudari <Pudarijagan47@gmail.com>
2026-03-11 20:29:38 -0500

    Initial commit: Cloud Native DevOps Platform on Kubernetes

---
+++ app/app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from prometheus_client import Counter, generate_latest
+
+app = Flask(__name__)
+
+REQUEST_COUNT = Counter('app_requests_total', 'Total App Requests')
+
+@app.route('/')
+def home():
+    REQUEST_COUNT.inc()
+    return "Cloud Native DevOps Platform Running!"
+
+@app.route('/metrics')
+def metrics():
+    return generate_latest()
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
